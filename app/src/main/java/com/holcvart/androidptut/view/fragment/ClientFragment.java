f10d6a39d4cec4302297f2b31a44d1738e81d230
CocoH11 <55780171+CocoH11@users.noreply.github.com>
2020-11-30 16:01:18 +0100

    The ClientFragment retrieves the recyclerView in the xml files and implements an adapter which will fill this recyclerView

--- app/src/main/java/com/holcvart/androidptut/view/fragment/ClientFragment.java
+++ app/src/main/java/com/holcvart/androidptut/view/fragment/ClientFragment.java
@@ -11,26 +11,30 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.holcvart.androidptut.MainActivity;
 import com.holcvart.androidptut.R;
+import com.holcvart.androidptut.model.repository.ClientRepository;
 import com.holcvart.androidptut.view.model.ClientViewModel;
+import com.holcvart.androidptut.view.model.ClientViewModelFactory;
+import com.holcvart.androidptut.view.recycler_view.ClientListAdapter;
 
 public class ClientFragment extends Fragment {
 
     private ClientViewModel clientViewModel;
+    private RecyclerView recyclerView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ClientViewModelFactory viewModelFactory= new ClientViewModelFactory(new ClientRepository(((MainActivity)requireActivity()).getDatabase()));
         clientViewModel =
-                new ViewModelProvider(this).get(ClientViewModel.class);
+                new ViewModelProvider(this, viewModelFactory).get(ClientViewModel.class);
         View root = inflater.inflate(R.layout.fragment_client, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        clientViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        recyclerView = root.findViewById(R.id.clientRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        recyclerView.setAdapter(new ClientListAdapter(clientViewModel.findAll()));
         return root;
     }
 }

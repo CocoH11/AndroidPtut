59e68e5be7adc0144615b82e8ab3126ccb67cb61
CocoH11 <55780171+CocoH11@users.noreply.github.com>
2020-11-30 14:12:47 +0100

    Contract and Helper to create the database implemented with a first table: client

---
+++ app/src/main/java/com/holcvart/androidptut/model/database/PhoneRepairManagementContract.java
@@ -0,0 +1,34 @@
+package com.holcvart.androidptut.model.database;
+
+import android.provider.BaseColumns;
+
+public final class PhoneRepairManagementContract {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "PhoneRepairManagement.db";
+
+    private PhoneRepairManagementContract(){}
+
+    public final static class Client implements BaseColumns {
+        public final static String TABLE_NAME="client";
+        public final static String COLUMN_NAME_FIRST_NAME="firstName";
+        public final static String COLUMN_NAME_NAME="name";
+        public final static String COLUMN_NAME_EMAIL="email";
+        public final static String COLUMN_NAME_PHONE="phone";
+        public final static String COLUMN_NAME_ADDRESS="address";
+
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE "+TABLE_NAME+"("+
+                        _ID + " INTEGER PRIMARY KEY," +
+                        COLUMN_NAME_NAME + "TEXT," +
+                        COLUMN_NAME_FIRST_NAME + "TEXT," +
+                        COLUMN_NAME_EMAIL + "TEXT," +
+                        COLUMN_NAME_PHONE + "TEXT," +
+                        COLUMN_NAME_ADDRESS + "TEXT);"
+                ;
+
+        public static final String SQL_DELETE_TABLE =
+                "DELETE TABLE IF EXISTS "+TABLE_NAME
+                ;
+
+    }
+}
---
+++ app/src/main/java/com/holcvart/androidptut/model/database/PhoneRepairManagementDBHelper.java
@@ -0,0 +1,27 @@
+package com.holcvart.androidptut.model.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.holcvart.androidptut.model.database.PhoneRepairManagementContract.DATABASE_NAME;
+import static com.holcvart.androidptut.model.database.PhoneRepairManagementContract.DATABASE_VERSION;
+
+public class PhoneRepairManagementDBHelper extends SQLiteOpenHelper {
+
+    public PhoneRepairManagementDBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(PhoneRepairManagementContract.Client.SQL_CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(PhoneRepairManagementContract.Client.SQL_DELETE_TABLE);
+        onCreate(db);
+    }
+
+}

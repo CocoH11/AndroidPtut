cac7804d1c357aa203635501381353db4b0c39ad
Nicolas BOEHRER <nboehrer@laposte.net>
2021-02-19 10:22:01 +0100

    modif paramètres

--- app/src/main/java/com/holcvart/androidptut/view/SettingsActivity.java
+++ app/src/main/java/com/holcvart/androidptut/view/SettingsActivity.java
@@ -1,40 +1,22 @@
 package com.holcvart.androidptut.view;
 
-import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
-import com.holcvart.androidptut.DatabaseTest;
-import com.holcvart.androidptut.MainActivity;
 import com.holcvart.androidptut.R;
-import com.holcvart.androidptut.model.database.PhoneRepairManagementDBHelper;
 
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private FloatingActionButton floatingActionButton;
-    private ActionBar actionBar;
 
     private EditText editTextName;
     private EditText editTextAdresse;
